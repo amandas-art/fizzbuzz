aaea813e5c2d69e0b79f42ead7ace451f104053b
amandas-art <amanda.sprukte@gmail.com>
2025-12-28 21:50:46 +0200

    add

---
+++ script.js
@@ -0,0 +1,49 @@
+const grid = document.getElementById("grid");
+const generateBtn = document.getElementById("generateBtn");
+const clearBtn = document.getElementById("clearBtn");
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+function clearGrid() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+}
+function generateFizzBuzz() {
+  clearGrid();
+
+  const start = parseInt(document.getElementById("startValue").value);
+  const end = parseInt(document.getElementById("endValue").value);
+  const fizz = parseInt(document.getElementById("fizzValue").value);
+  const buzz = parseInt(document.getElementById("buzzValue").value);
+
+  for (let i = start; i <= end; i++) {
+    const div = document.createElement("div");
+    div.id = `myid${i}`;
+    div.classList.add("box");
+
+    if (i % fizz === 0 && i % buzz === 0) {
+      div.classList.add("fizzbuzz");
+      div.innerText = "FizzBuzz";
+    } else if (i % fizz === 0) {
+      div.classList.add("fizz");
+      div.innerText = "Fizz";
+    } else if (i % buzz === 0) {
+      div.classList.add("buzz");
+      div.innerText = "Buzz";
+    } else {
+      div.classList.add("number");
+      div.innerText = i;
+    }
+
+    grid.appendChild(div);
+  }
+}
+generateBtn.addEventListener("click", generateFizzBuzz);
+clearBtn.addEventListener("click", clearGrid);
+
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// Auto-generate on load
+generateFizzBuzz();
